2ebbc88be13d09d95ff31eb6b6e66742f5d16958
bhaskar-gusain <114845812+bhaskar-gusain@users.noreply.github.com>
2025-10-07 19:01:26 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
-for (let index = 0; index < 5; index++) {
-    basic.showString("Hi vatsal")
-}
+basic.showNumber(0)
 let list = [
-10,
-20,
-30,
-40,
-50
+0,
+1,
+0,
+0,
+0
 ]
 for (let index = 0; index <= 4; index++) {
     basic.showNumber(list[index])
