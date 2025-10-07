6285528daecf4f86e9b19e834fb067106e08e463
bhaskar-gusain <114845812+bhaskar-gusain@users.noreply.github.com>
2025-10-07 18:33:13 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+for (let index = 0; index < 5; index++) {
+    basic.showString("Hi vatsal")
+}
+let list = [
+10,
+20,
+30,
+40,
+50
+]
+for (let index = 0; index <= 4; index++) {
+    basic.showNumber(index)
+}
